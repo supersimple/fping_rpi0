5acf71e5941405e4c88063db8d48cd9e8e16e85d
Todd Resudek <toddresudek@gmail.com>
2021-02-25 21:09:22 -0700

    update mixfile

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi0.MixProject do
+defmodule FpingRpi0.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi0
+  @github_organization "supersimple"
+  @app :fping_rpi0
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
